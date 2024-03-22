8471d179f167d410f7c56c9eda4bdda6be0b127c
Jordi Garreta <jordigarreta11@gmail.com>
2024-03-22 10:28:22 +0100

    improve themeContextProvider

--- components/theme.tsx
+++ components/theme.tsx
@@ -1,10 +1,14 @@
 'use client';
+import { createContext, useContext, useState, ReactNode } from 'react';
 
-import { createContext, useContext, useState } from 'react';
+interface ThemeContextType {
+  menu: boolean;
+  setMenu: (menu: boolean) => void;
+}
 
-const ThemeContext = createContext({});
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export const ThemeContextProvider = ({ children }) => {
+export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
   const [menu, setMenu] = useState(false);
 
   return (
@@ -19,4 +23,13 @@ export const ThemeContextProvider = ({ children }) => {
   );
 };
 
-export const useThemeContext = () => useContext(ThemeContext);
+// export const useThemeContext = () => useContext(ThemeContext);
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useThemeContext must be used within a ThemeContextProvider');
+  }
+  return context;
+};
+
